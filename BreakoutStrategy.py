2b8cd0e4e1a1d8876d642483e4e316e0744e23d1
lolke <lolkedijkstra@gmail.com>
2024-08-05 21:28:24 +0200

    changed zone_height from abs to rel

--- BreakoutStrategy.py
+++ BreakoutStrategy.py
@@ -11,137 +11,136 @@ logger = logging.getLogger()
 
 
 def log_signal(level, i, data, signal):
-    name = "BUY" if signal == Signal.BUY else "SELL" if signal == Signal.SELL else "NONE" 
+    name = "BUY" if signal == Signal.BUY else "SELL" if signal == Signal.SELL else "NONE"
     logger.log(level, f'{i}, {data.open.array[i]}, {data.high.array[i]}, {data.low.array[i]}, {data.close.array[i]}, {data.volume.array[i]}, {name}')
 
 def log_signals(level, data, signals, s):
     if level < logger.level:
         return
-    
+
     sz = len(signals)
-    
+
     match(s):
         case Signal.BUY:
             for i in range(0, sz):
                 if Signal.BUY == int(signals[i]):
                     log_signal(level, i, data, signals[i])
-                    
+
         case Signal.SELL:
             for i in range(0, sz):
                 if Signal.SELL == int(signals[i]):
                     log_signal(level, i, data, signals[i])
-                    
+
         case Signal.EITHER:
             for i in range(0, sz):
                 if int(signals[i] in [Signal.BUY, Signal.SELL] ):
                     log_signal(level, i, data, signals[i])
-                   
+
 
 
 from array import array
 
 class BreakoutStrategy(Strategy):
-    
+
     LOG_INIT = False
     LOG_ORDERS = False
 
     LOG_LEVEL = logging.INFO
-    
+
     LONG = True
     SHORT = False
-    
+
     VERBOSE = False
 
     run_nr = 1
-    
+
     #
     # input parameters for the BreakoutStrategy class
-    # these parameters need to be set in the main program and 
+    # these parameters need to be set in the main program and
     # can be used with optimize to get the optimal value combinations
     #
     params = dict(
         ticker      = None,
-        tp_sl_ratio = 0.0,    
+        tp_sl_ratio = 0.0,
         sl_distance = 0.0,         # distance % below or above current price depending on direction
-        
+
         #
         # the following parameters will be received from cerebro
         #
-        
-        backcandles  = 0,     
-        gap_window   = 0, 
+
+        backcandles  = 0,
+        gap_window   = 0,
         zone_height  = 0.0,
         breakout_f   = 0.0,
-        
+
     #    pivot_window = 0,     <= not used now, pivots are calculated outside
-        pivots       = [int]               
-    )   
-     
-     
+        pivots       = [int]
+    )
+
+
     #
     # BreakoutStrategy.log
     #
-    def log(self, txt, dt=None):        
+    def log(self, txt, dt=None):
         dt = dt or self.data.datetime.date(0)
         logger.log(BreakoutStrategy.LOG_LEVEL, '%s, %s' % (dt.isoformat(), txt))
-    
-    
+
+
     #
     # BreakoutStrategy.__init__
     #
-    def __init__(self):  
-                       
+    def __init__(self):
+
         print(f"calculating signals, run no: {BreakoutStrategy.run_nr}")
-        
+
         # keep track of pending orders
         self.order = None
         self.buyprice = None
         self.buycomm = None
-            
+
         #self.open_positions = 0  the idea is to allow multiple orders in parallel up to MAX_OPEN
-               
-               
+
+
         #self.data['ema'] = bt.indicators.ExponentialMovingAverage
         #self.data['ema_signal'] = is_trend(self.data, backcandles=10)
-        
-        bc = self.params.backcandles 
-        gw = self.params.gap_window
-        zh = self.params.zone_height  
-        bf = self.params.breakout_f    
-        logger.debug(f'backcandles: {bc}, gap window: {gw}, zone height: {zh}, breakout_factor: {bf}')    
+
+
+        self.sl_dist = self.params.sl_distance     # stop distance as fraction of last close
+        self.tp_sl   = self.params.tp_sl_ratio     # w/l ratio
+
 
         # get signals and reset signal index
         self.signal_idx = 0
         sz = self.data.buflen()
-        self.signals = array('i', [0] * sz)             
+        self.signals = array('i', [0] * sz)
         for idx in range(0, sz):
             self.signals[idx] = algo.calc_signal(
-                                    data        = self.data, 
-                                    candle_idx  = idx,  
-                                    backcandles = bc, 
+                                    data        = self.data,
+                                    candle_idx  = idx,
+                                    backcandles = bc,
                                     gap_window  = gw,
                                     pivots      = self.params.pivots,
                                     zone_height = zh,
                                     breakout_f  = bf
                                     )
-    
-             
+
+
         log_signals(logging.DEBUG, self.data, self.signals,  Signal.BUY | Signal.SELL )
-        
- 
+
+
     def accept_short(self) -> bool:
         return BreakoutStrategy.SHORT
-    
+
     def accept_long(self) -> bool:
         return BreakoutStrategy.LONG
-    
+
     def get_signal(self) -> int:
         return self.signals[self.signal_idx]
-   
-    
+
+
     def start(self):
         BreakoutStrategy.run_nr = BreakoutStrategy.run_nr + 1
-    
+
 
     def next(self):
         # log current close
@@ -156,59 +155,57 @@ class BreakoutStrategy(Strategy):
             return
 
         if not self.position:   # Check if we are in the market
-        #if self.open_positions <= MAX_OPEN:  
+        #if self.open_positions <= MAX_OPEN:
             close = self.data.close[0]          # last close
-            s_fac = self.params.sl_distance     # stop distance as fraction of last close
-            r     = self.params.tp_sl_ratio     # w/l ratio
-            
-            match self.get_signal(): 
-                
-                case Signal.SELL: 
+
+            match self.get_signal():
+
+                case Signal.SELL:
                     if self.accept_long():
-                        stop1  = close * (1.0 + s_fac)
-                        limit1 = close * (1.0 - r * s_fac)   
-                        
-                        self.log(f'OPEN SELL [close={close:6.4f}, stoploss={stop1:6.4f}, limit={limit1:6.4f}]')               
+                        stop1  = close * (1.0 + self.sl_dist)
+                        limit1 = close * (1.0 - self.tp_sl * self.sl_dist)
+
+                        self.log(f'OPEN SELL [close={close:6.4f}, stoploss={stop1:6.4f}, limit={limit1:6.4f}]')
                         self.order = self.sell_bracket(limitprice=limit1, stopprice=stop1, size=None)
-                        
+
                 case Signal.BUY:
                     if self.accept_short():
-                        stop1  = close * (1.0 - s_fac)
-                        limit1 = close * (1.0 + r * s_fac)
-                        
-                        self.log(f'OPEN BUY [close={close:6.4f}, stoploss={stop1:6.4f}, limit={limit1:6.4f}]')               
+                        stop1  = close * (1.0 - self.sl_dist)
+                        limit1 = close * (1.0 + self.tp_sl * self.sl_dist)
+
+                        self.log(f'OPEN BUY [close={close:6.4f}, stoploss={stop1:6.4f}, limit={limit1:6.4f}]')
                         self.order = self.buy_bracket(limitprice=limit1, stopprice=stop1, size=None)
-                        #self.order = self.buy_bracket(exectype=bt.Order.StopTrail, trailpercent=2*s_fac, limitprice=limit1)               
+                        #self.order = self.buy_bracket(exectype=bt.Order.StopTrail, trailpercent=2*s_fac, limitprice=limit1)
                 case 0:
                     pass
                 case _:
                     raise ValueError(f'Signal: {self.get_signal()} is invalid. Should be either 0, 1, or 2')
-         
-        else:  # we have a current position
-            pass 
-         
-        # incr signal index 
-        self.signal_idx = self.signal_idx+1
-        
-        
 
-    def notify_order(self, order):        
-        
+        else:  # we have a current position
+            pass
+
+        # incr signal index
+        self.signal_idx = self.signal_idx+1
+
+
+
+    def notify_order(self, order):
+
         if order.status in [order.Submitted, order.Accepted]:
             return
-                  
-        
+
+
         # order completed
         if order.status in [order.Completed]:
             # BUY
-            if order.isbuy():       
+            if order.isbuy():
                 self.log(f'BUY EXECUTED [Price: {order.executed.price:.2f}, Cost: {order.executed.value:.2f}, Comm: {order.executed.comm:.2f}]')
-                   
+
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
-                
-            # SELL    
-            elif order.issell():    
+
+            # SELL
+            elif order.issell():
                 self.log(f'SELL EXECUTED [Price: {order.executed.price:.2f}, Cost: {order.executed.value:.2f}, Comm: {order.executed.comm:.2f}]')
 
             #self.open_positions = self.open_positions +1
@@ -221,11 +218,11 @@ class BreakoutStrategy(Strategy):
             self.log('Order Margin')
         elif order.status == order.Rejected:
             self.log('Order Rejected')
-            
+
         # Write down: no pending order
         self.order = None
-       
-        
+
+
     def notify_trade(self, trade):
         if not trade.isclosed:
             return
@@ -233,8 +230,8 @@ class BreakoutStrategy(Strategy):
         #self.open_positions = self.open_positions -2
         if BreakoutStrategy.VERBOSE:
             print(f'{trade.baropen:5d}, {bt.num2date(trade.dtopen)}, {trade.barclose:5d}, {bt.num2date(trade.dtclose)}, {trade.pnl:8.3f}, {trade.pnlcomm:8.3f}')
-            
-        self.log(f'OPERATION PROFIT, GROSS {trade.pnl:8.3f}, NET {trade.pnlcomm:8.3f}\n******')
-    
 
-       
+        self.log(f'OPERATION PROFIT, GROSS {trade.pnl:8.3f}, NET {trade.pnlcomm:8.3f}\n******')
+
+
+
--- algo.py
+++ algo.py
@@ -8,36 +8,36 @@ from pivot import Pivot
 
 from backtrader import lineseries
 
-class algo:   
-    
+class algo:
+
     @staticmethod
-    def is_trend(close:list[float], open:list[float], ema:list[float], backcandles:int=10) -> list[int]:    
+    def is_trend(close:list[float], open:list[float], ema:list[float], backcandles:int=10) -> list[int]:
         ema_signal = [0]*len(close)
 
         for idx in range(backcandles, len(close)):
             upt = 2 # 0x0010
             dnt = 1 # 0x0001
-            
+
             r = range(idx-backcandles, idx+1)
             for i in r:
                 if max(open[i], close[i]) > ema[i]:
                     dnt = 0
                     break
-                
-            for i in r:               
+
+            for i in r:
                 if min(open[i], close[i]) < ema[i]:
                     upt = 0
                     break
-                    
+
             ema_signal[idx] = upt | dnt
-            
+
         return ema_signal
-    
+
 
     @staticmethod
     def slice(data: lineseries, b: int, e: int):
         return data.get(ago=e-data.buflen(), size=e-b)
-      
+
 
     @staticmethod
     def get_pivots(highlow: int, data: list[float], pivots: list[int]) -> list[float]:
@@ -45,7 +45,7 @@ class algo:
         for i in range(0, len(data)):
             if pivots[i] == highlow:
                 values.append(data[i])
-                
+
         return values
 
     @staticmethod
@@ -55,27 +55,27 @@ class algo:
     @staticmethod
     def get_pivots_low(data: list[float], pivots: list[int]) -> list[float]:
         return algo.get_pivots(Pivot.LOW, data, pivots)
-    
-    
+
+
     import typing
     @staticmethod
     def _check_breakout(f_breakout_test: typing.Callable[[float, float, float], int], values: list[float], zone_height: float, cclose: float) -> int:
-        
+
         # helper function: check if pivots form a zone
         #def _is_zone(values , mean, height):
-        def _is_zone(values: list[float], mean: float, height: float):
+        def _is_zone(values: list[float], mean: float, zheight: float):
             for value in values:
-                if abs(value-mean) > height:
-                    return False   
+                if abs(value-mean) > zheight * mean:
+                    return False
             return True
 
 
-        # main function body    
+        # main function body
         mean = statistics.mean(values)
         if _is_zone(values, mean, zone_height):
-            return f_breakout_test(mean, cclose, zone_height)           
-        
-        return Signal.NONE 
+            return f_breakout_test(mean, cclose, zone_height)
+
+        return Signal.NONE
 
 
 
@@ -85,57 +85,57 @@ class algo:
     # backcandles   the width of the window of analysis
     # gap_window    gap must be >= pivot window to make sure pivot window doesn;t extend beyond current candle
     # pivots        the array holding the pivots
-    # zone_height   the absolute price fluctuation  
+    # zone_height   the relative price fluctuation
     #
     #from backtrader.feed import Database
-   
+
     @staticmethod
-    def calc_signal(data, candle_idx: int, backcandles: int, gap_window: int, pivots: list[int], zone_height: float, breakout_f: float) -> int: 
+    def calc_signal(data, candle_idx: int, backcandles: int, gap_window: int, pivots: list[int], zone_height: float, breakout_f: float) -> int:
         #print( backcandles, gap_window, zone_height)
-        
-        # gap_window must be >= pivot window to avoid look ahead bias     
+
+        # gap_window must be >= pivot window to avoid look ahead bias
         begin = candle_idx - backcandles - gap_window
-        end   = candle_idx - gap_window   
-        
+        end   = candle_idx - gap_window
+
         if begin < 0 or candle_idx + gap_window >= data.buflen():
             return 0
-        
-                
+
+
         _F = breakout_f     # breakout factor
         _N = 3              # number of bounces
-        sig = Signal.NONE    
-            
-        cclose = data.close.array[candle_idx]                     
+        sig = Signal.NONE
+
+        cclose = data.close.array[candle_idx]
 
         lows = data.low.array
         pvts = algo.get_pivots_low(data=lows[begin:end], pivots=pivots[begin:end])[-_N:]
-        
+
         if _N == len(pvts):
-            sig = algo._check_breakout(                                             
-                    lambda mean, cclose, zheight: (Signal.SELL if (mean - cclose) > zheight * _F else 0), 
-                    pvts, 
-                    zone_height, 
+            sig = algo._check_breakout(
+                    lambda mean, cclose, zheight: (Signal.SELL if (mean - cclose) > zheight * mean * _F else 0),
+                    pvts,
+                    zone_height,
                     cclose
                 )
-        
-            if sig != Signal.NONE: 
+
+            if sig != Signal.NONE:
                 #print(f"SELL SIGNAL: {candle_idx}")
                 return sig
-      
-      
+
+
         highs = data.high.array
         pvts = algo.get_pivots_high(data=highs[begin:end], pivots=pivots[begin:end])[-_N:]
-                
-        if _N == len(pvts):      
+
+        if _N == len(pvts):
             sig = algo._check_breakout(
-                lambda mean, cclose, zheight: (Signal.BUY if (cclose - mean) > zheight * _F else 0),   
-                pvts, 
-                zone_height, 
+                lambda mean, cclose, zheight: (Signal.BUY if (cclose - mean) > zheight * mean * _F else 0),
+                pvts,
+                zone_height,
                 cclose
             )
- 
-            #if sig != Signal.NONE: 
+
+            #if sig != Signal.NONE:
             #    print(f"BUY SIGNAL: {candle_idx}")
-            
-            
+
+
         return sig
